784a7c1ff7dad8681467db4e1d2889e0d12b58fd
Mission Kumar <missionk935@gmail.com>
2026-06-05 14:10:29 +0530

    first comment

--- .next/dev/static/chunks/app_layout_tsx_004glpo._.js
+++ .next/dev/static/chunks/app_layout_tsx_004glpo._.js
@@ -1,5 +1,5 @@
 (globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__06.-pfn._.css","static/chunks/node_modules_06ai7lx._.js","static/chunks/_0--r-wa._.js"],
+    chunks: ["static/chunks/[root-of-the-server]__06.-pfn._.css","static/chunks/node_modules_next_0h9-s3l._.js","static/chunks/components_Header_tsx_0xxg71t._.js"],
     source: "dynamic"
 });
--- lib/coingeckoChart.ts
+++
@@ -1,7 +0,0 @@
-// lib/coingeckoChart.ts
-export async function fetchChartData(coinId: string) {
-  const res = await fetch(
-    `https://api.coingecko.com/api/v3/coins/${coinId}/ohlc?vs_currency=usd&days=1`
-  );
-  return res.json();
-}
